7ba8e2ffc71c76b3e095dfc309f148ac569795c6
nayyapu <nayyapu@DIN73003041.corp.capgemini.com>
2018-11-23 14:20:57 +0530

    commited

---
+++ Day1-BDD/src/main/java/codedemo/MainClass.java
@@ -0,0 +1,16 @@
+package codedemo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello!How are you?");
+		
+		greetings();
+	}
+	
+	public static void greetings() {
+		System.out.println("I'm from Greetings");
+	}
+
+}
